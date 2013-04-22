1aa04a4824225887d7ad5e82aa8b24357563dcb5
Tyson Tate <tyson@tysontate.com>
2013-04-22 10:06:06 -0700

    Update readme, clean up formatting.

--- README.md
+++ README.md
@@ -1,15 +1,17 @@
 Zookeeper PasswordAuthenticationProvider
 ========================================
 
-PasswordAuthenticationProvider is (will be) a Zookeeper
-AuthenticationProvider that allows connection-level authentication of
-Zookeeper clients. Unlike the built-in DigestAuthenticationProvider,
-PasswordAuthenticationProvider authenticates connections at the time that they
-connect, not the time they run commands.
+PasswordAuthenticationProvider is a [Zookeeper AuthenticationProvider][zk_auth]
+that allows connection-level authentication of Zookeeper clients with a single
+string password. Unlike the built-in DigestAuthenticationProvider,
+PasswordAuthenticationProvider authenticates clients at the time that they
+connect, not only when they send read / write commands to Zookeeper.
 
-TODO
-----
+Only one password is allowed. The base 64 encoded SHA1 digest of the password
+should be set as a Java system property as
+"zookeeper.PasswordAuthenticationProvider.passwordDigest".
 
-* Don't use the bespoke base64 algo
-* Add tests
+Currently only compatible with Zookeeper 3.3.x.
+
+[zk_auth]: http://zookeeper.apache.org/doc/r3.3.5/zookeeperAdmin.html#sc_authOptions
 
--- src/main/java/com/stovepipestudios/zookeeper/server/auth/PasswordAuthenticationProvider.java
+++ src/main/java/com/stovepipestudios/zookeeper/server/auth/PasswordAuthenticationProvider.java
@@ -12,36 +12,32 @@ import org.apache.zookeeper.data.Id;
 import org.apache.zookeeper.server.ServerCnxn;
 
 public class PasswordAuthenticationProvider implements AuthenticationProvider {
-    private static final Logger LOG = Logger.getLogger(PasswordAuthenticationProvider.class);
+    private static final Logger LOG = Logger.getLogger( PasswordAuthenticationProvider.class );
 
-    /** specify a command line property with key of
-     * "zookeeper.PasswordAuthenticationProvider.passwordDigest" and value of
-     * `base64encoded( SHA1( password ) )`
-     */
+    // Specify a command line property with key of
+    // "zookeeper.PasswordAuthenticationProvider.passwordDigest" and value of
+    // `base64encoded( SHA1( password ) )`
     private final static String masterDigest = System.getProperty("zookeeper.PasswordAuthenticationProvider.passwordDigest");
 
     public String getScheme() {
         return "password";
     }
 
-    static public String generateDigest(String password) throws NoSuchAlgorithmException {
-        byte digest[] = MessageDigest.getInstance("SHA1").digest(password.getBytes());
+    static public String generateDigest( String password ) throws NoSuchAlgorithmException {
+        byte digest[] = MessageDigest.getInstance( "SHA1" ).digest( password.getBytes() );
         return new String( Base64.encodeBase64( digest ) );
     }
 
-    public KeeperException.Code handleAuthentication(ServerCnxn cnxn, byte[] authData)
-    {
-        String givenPassword = new String(authData);
+    public KeeperException.Code handleAuthentication( ServerCnxn cnxn, byte[] password ) {
+        String givenPassword = new String( password );
         try {
-            String givenDigest = generateDigest(givenPassword);
-            if (givenDigest.equals(masterDigest)) {
-              cnxn.getAuthInfo().add(new Id(getScheme(), givenDigest));
+            String givenDigest = generateDigest( givenPassword );
+            if ( givenDigest.equals( masterDigest ) ) {
+              cnxn.getAuthInfo().add( new Id( getScheme(), givenDigest ) );
               return KeeperException.Code.OK;
-            } else {
-              return KeeperException.Code.AUTHFAILED;
             }
-        } catch (NoSuchAlgorithmException e) {
-          LOG.error("Missing algorithm",e);
+        } catch ( NoSuchAlgorithmException e ) {
+            LOG.error( "Missing algorithm", e );
         }
         return KeeperException.Code.AUTHFAILED;
     }
@@ -55,7 +51,7 @@ public class PasswordAuthenticationProvider implements AuthenticationProvider {
     }
 
     public boolean matches(String id, String aclExpr) {
-        return id.equals(aclExpr);
+        return id.equals( aclExpr );
     }
 }
 
